efc4a1bcdfbb3e7aa51a1501d82f490be139a019
Harman6282 <harmans6282@gmail.com>
2025-01-01 22:45:53 +0530

    Schema added

--- social-next/src/app/layout.tsx
+++ social-next/src/app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-import { ThemeProvider } from "@/components/theme-provider"
+import { ThemeProvider } from "@/components/theme-provider";
+import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,8 +26,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-        <ClerkProvider>
+    <html lang="en" suppressHydrationWarning>
+      <ClerkProvider>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
@@ -34,12 +35,22 @@ export default function RootLayout({
             attribute="class"
             defaultTheme="system"
             enableSystem
-            disableTransitionOnChange                                  
+            disableTransitionOnChange
           >
-            {children}
+            <div className="min-h-screen">
+              <Navbar />
+              <main className="py-8">
+                <div className="max-w-7xl mx-auto px-4">
+                  <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
+                    <div className="hidden lg:block lg:col-span-3">sidebar</div>
+                    <div className="lg:col-span-9">{children}</div>
+                  </div>
+                </div>
+              </main>
+            </div>
           </ThemeProvider>
         </body>
-    </ClerkProvider>
-      </html>
+      </ClerkProvider>
+    </html>
   );
 }
--- social-next/src/app/page.tsx
+++ social-next/src/app/page.tsx
@@ -1,25 +1,11 @@
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import ModeToggle from "@/components/ModeToggle";
-
 
 export default function Home() {
   return (
     <div>
-      <SignedOut>
-        <SignInButton mode="modal">
-          <Button>
-            Sign in
-          </Button>
-        </SignInButton>
-      </SignedOut>
-
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-
- <ModeToggle />
-
+     home page
     </div>
   );
 }
