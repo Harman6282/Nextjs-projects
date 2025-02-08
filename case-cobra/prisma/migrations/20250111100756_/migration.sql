703d0f95177587f94250d3d69f0acd204532f64f
Harman6282 <harmans6282@gmail.com>
2025-02-08 12:53:45 +0530

    waste deleted

--- case-cobra/prisma/migrations/20250111100756_/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Configuration" (
-    "id" TEXT NOT NULL,
-    "width" INTEGER NOT NULL,
-    "height" INTEGER NOT NULL,
-    "croppedImageUrl" TEXT,
-
-    CONSTRAINT "Configuration_pkey" PRIMARY KEY ("id")
-);
