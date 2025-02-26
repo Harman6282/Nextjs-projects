7fa6d4396f93952a888ec849e04210b75ee97d67
Harman6282 <harmans6282@gmail.com>
2025-02-26 21:30:29 +0530

    updates

--- reels-pro/models/User.ts
+++ reels-pro/models/User.ts
@@ -1,5 +1,4 @@
 import mongoose, { model, models, Schema } from "mongoose";
-
 import bcrypt from "bcryptjs";
 
 export interface IUser {
@@ -11,7 +10,7 @@ export interface IUser {
 }
 
 const userSchema = new Schema<IUser>(
-  {
+  {      
     email: {
       type: String,
       required: true,
