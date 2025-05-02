43e6c77e1e7074f37ad783a37780116ce17b32fa
Harman6282 <harmans6282@gmail.com>
2025-05-02 11:14:28 +0530

    todo init

--- next-rev/app/page.tsx
+++ next-rev/app/page.tsx
@@ -1,9 +1,19 @@
-import Image from "next/image";
+import { prisma } from "./utils/db";
+async function getData(){
 
-export default function Home() {
+  
+} 
+
+export default async function Home() {
+  //  const data = await getData();
+  
   return (
-   <div>
-    hello world this is harman singh here
+   <div className="py-6">
+     <h1 className="text-3xl font-bold tracking-tight mb-8">Latest Posts</h1>
+
+     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      
+     </div>
    </div>
   );
 }
